3e99a381e5f4d638415473377ac66ccfa6458733
Turukmoorea aka R2Turuk2 <mail@turukmoorea.ch>
2024-08-09 16:49:14 +0200

    Update script.maintain.userhome_directories.renew.ps1

--- maintain.userhome_directories/script.maintain.userhome_directories.renew.ps1
+++ maintain.userhome_directories/script.maintain.userhome_directories.renew.ps1
@@ -1,4 +1,4 @@
-<#header===========================================================================================
+<#header ==========================================================================================
 .SCRIPTNAME
     script.maintain.userhome_directories.ps1
 
@@ -14,49 +14,68 @@ and provides the option to delete empty directories or those with unresolved sec
 It offers an interactive interface that allows users to decide whether certain directories should be deleted.
 #>
 
-#param=============================================================================================
+#param ============================================================================================
 param (
-    [switch]$help,  # Switch to trigger the helppage.
-    [switch]$h,  # Switch to trigger the helppage.
+    [switch]$help, # Switch to trigger the helppage.
+    [switch]$h, # Switch to trigger the helppage.
+    [switch]$debug, # Switch to trigger the debug mode.
 
-    [string]$mode = "maintain",  # Mode of operation, with a default value of "maintain".
-    [string]$parentPath = $(Get-Location),  # The parent directory path to operate on.
-    [bool]$prompt = $true,  # Flag indicating whether to prompt the user for confirmation.
+    [string]$mode = "maintain", # Mode of operation, with a default value of "maintain".
+    [string]$parentPath = $(Get-Location), # The parent directory path to operate on.
+    [bool]$prompt = $true, # Flag indicating whether to prompt the user for confirmation.
     [string]$emptyDirectories = "retain"  # Specifies how to handle empty directories, defaulting to "retain".
 )
 
-#Helppage==========================================================================================
+$debugVarTable = @() # Initializes an empty, global debug table.
+
+#Helppage =========================================================================================
 # Function to display the help page.
 function Show-Helppage {
-    Write-Output "Usage: <scriptname> [parameters]"
     Write-Output ""
+    Write-Output "#Helppage =========================================================================================="
+    Write-Output "Usage: <scriptname> [parameters]"
     
     $table = @()
     
     # Add rows to the table
-    $table += [PSCustomObject]@{Parameter="-help, -h   "; Options=""; Description="Display this help page."}
-    $table += [PSCustomObject]@{Parameter="-parentPath <string>   "; Options=""; Description="The absolute or relative parent directory path to access. Default is current Directory."}
-    $table += [PSCustomObject]@{Parameter="-prompt <bool>   "; Options="0, `$false, 1, `$true   "; Description="Flag indicating whether to prompt the user for confirmation. Default is true."}
-    $table += [PSCustomObject]@{Parameter="-emptyDirectories <string>   "; Options="retain, delete   "; Description="Specifies how to handle empty directories. Default is 'retain'."}
+    $table += [PSCustomObject]@{Parameter = "-help, -h   "; Options = ""; Default = ""; Description = "Display this help page." }
+    $table += [PSCustomObject]@{Parameter = "-debug   "; Options = ""; Default = ""; Description = "Display all variable (step-by-step)" }
+    $table += [PSCustomObject]@{Parameter = "-mode <string>   "; Options = "maintain, show, show all   "; Default = "maintain"; Description = "Select the script mode." }
+    $table += [PSCustomObject]@{Parameter = "-parentPath <string>   "; Options = ""; Default = "current path   "; Description = "The absolute or relative parent directory path to access." }
+    $table += [PSCustomObject]@{Parameter = "-prompt <bool>   "; Options = "0, `$false, 1, `$true   "; Default = "true   "; Description = "Flag indicating whether to prompt the user for confirmation." }
+    $table += [PSCustomObject]@{Parameter = "-emptyDirectories <string>   "; Options = "retain, delete   "; Default = "retain   "; Description = "Specifies how to handle empty directories." }
     
     # Display the table
     $table | Format-Table -AutoSize
+}
+
+function Show-AllExitCode {
+    Write-Output ""
+    Write-Output "#All Exit-Codes ===================================================================================="
+    $table = @()
     
-    exit
+    # Add rows to the table
+    $table += [PSCustomObject]@{Code = ""; Row = ""; Function = "" }
+    
+    # Display the table
+    $table | Format-Table -AutoSize
+    Write-Output ""
 }
 
 # Check for help parameters and display the help page if any are found.
 if ($help -or $h -or $question) {
     Show-Helppage
+    Show-AllExitCode
+    exit
 }
 
 
-#Ignore-List=======================================================================================
+#Ignore-List ======================================================================================
 
 function GET-IgnoreList {
     $ignoreList = [PSCustomObject]@{
-        SID  = @(
-            "S-1-1-0",  # Everyone
+        SID = @(
+            "S-1-1-0", # Everyone
             "S-1-5-18", # NT AUTHORITY\SYSTEM
             "S-1-5-32-544" # BUILTIN\Administrators
         )
@@ -70,19 +89,91 @@ function GET-IgnoreList {
 }
 
 
-#Functions=========================================================================================
+#Functions ========================================================================================
 
-#Resolve-AbsolutePath------------------------------------------------------------------------------
+#Debug-Function -----------------------------------------------------------------------------------
+function Write-DebugInfo {
+    param (
+        [string]$itemName,
+        $itemValue,
+        [string]$description
+    )
+    
+    # Add rows to the table
+    $debugVarTable += [PSCustomObject]@{Code = ""; Row = ""; Function = "" }
+}
+
+function Show-DebugInfo() {
+    if ($debug) {
+        Write-Host "#DEBUG-REPORT ======================================================================================"
+        Write-Host "all variables"
+
+        # Display the table
+        $debugVarTable | Format-Table -AutoSize
+    }
+}
+
+Write-DebugInfo -itemName "debug" -itemValue $debug -description "Initial script parameter value"
+Write-DebugInfo -itemName "mode" -itemValue $mode -description "Initial script parameter value"
+Write-DebugInfo -itemName "parentPath" -itemValue $parentPath -description "Initial script parameter value"
+Write-DebugInfo -itemName "prompt" -itemValue $prompt -description "Initial script parameter value"
+Write-DebugInfo -itemName "emptyDirectories" $emptyDirectories -itemValue -description "Initial script parameter value"
+
+#Resolve-AbsolutePath -----------------------------------------------------------------------------
 # Function to resolve and validate the provided path.
 function Resolve-AbsolutePath {
     param ([string]$inputPath)
 
-
+    # Check if the path is absolute. If not, resolve it to an absolute path.
+    if (-not [System.IO.Path]::IsPathRooted($inputPath)) {
+        try {
+            $resolvedPath = Resolve-Path -Path $inputPath -ErrorAction Stop
+            if ($null -ne $resolvedPath) {
+                $outputPath = $resolvedPath.ProviderPath
+            }
+        }
+        catch {
+            Write-Output "Error: Path does not exist."
+            exit
+        }
+    }
+    return $outputPath  # Return the resolved input path.
 }
 
+#Get-SubDirectories -------------------------------------------------------------------------------
+# Function to get the names of subdirectories within the specified base path.
+function Get-SubDirectories {
+    param ([string]$basePath)  # The base directory path.
+    return Get-ChildItem -Path $basePath -Directory | Select-Object -ExpandProperty Name
+}
 
+#Invoke-SubDirectories ----------------------------------------------------------------------------
+function Invoke-SubDirectories {
+    param (
+        [string]$basePath, # The base directory path.
+        [string[]]$childItems  # The subdirectory paths to validate.
+    )
 
-#Mainscript========================================================================================
+    # Arrays to hold valid and invalid paths.
+    $validPaths = @()
+    $invalidPaths = @()
+    
+    # Check each subdirectory path to see if it exists.
+    foreach ($childItem in $childItems) {
+        $fullPath = Join-Path -Path $basePath -ChildPath $childItems
+        if (Test-Path -Path $fullPath) {
+            $validPaths += $childItems
+        }
+        else {
+            $invalidPaths += $childItems
+        }
+    }
+}
+
+#Mainscript =======================================================================================
+if ($debug) {
+    Show-DebugInfo
+}
 
 
 switch ($mode) {
